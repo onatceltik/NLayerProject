df833828b6eef86116ed3311caf7da49de2a4ad7
onatceltik <64609605+onatceltik@users.noreply.github.com>
2020-08-27 11:25:18 +0300

    Add project files.

---
+++ UdemyNLayerProject.Core/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyNLayerProject.Core.Models
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+
+        // virtual: It will keep track of the changes on the category
+        public virtual Category Category { get; set; }
+    }
+}
