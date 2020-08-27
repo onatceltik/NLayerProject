7a616817b1505bb1f606496fc1f96d34db1abe25
onatceltik <64609605+onatceltik@users.noreply.github.com>
2020-08-27 21:43:34 +0300

    UnitOfWork is added and startup.cs is configured

---
+++ UdemyNLayerProject.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Repository;
+using UdemyNLayerProject.Core.UnitOfWork;
+using UdemyNLayerProject.Data.Repositories;
+
+namespace UdemyNLayerProject.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private ProductRepository _productRepository;
+        private CategoryRepository _categoryRepository;
+
+        // ?? means if there is no repo create a neww one
+        public IProductRepository Products => _productRepository
+            = _productRepository ?? new ProductRepository(_context);
+
+        public ICategoryRepository Categories => _categoryRepository
+            = _categoryRepository ?? new CategoryRepository(_context);
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        // Commit or SaveChanges
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
