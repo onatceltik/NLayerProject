fcd4ad33e61f9d45a660a25fe2cdfc98517393d3
onatceltik <64609605+onatceltik@users.noreply.github.com>
2020-08-27 17:30:41 +0300

    AppDbContext and ProductConfiguration classes are added

---
+++ UdemyNLayerProject.Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyNLayerProject.Core.Models;
+
+namespace UdemyNLayerProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base
+        (options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+
+        }
+    }
+}
---
+++ UdemyNLayerProject.Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using UdemyNLayerProject.Core.Models;
+
+namespace UdemyNLayerProject.Data.Configurations
+{
+    class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id); // makes Id primary key
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Stock).IsRequired();
+            builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(18,2)"); // total 18 char, after comma 2 char
+            builder.Property(x => x.InnerBarcode).HasMaxLength(50);
+        }
+    }
+}
